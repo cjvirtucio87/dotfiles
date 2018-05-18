d702aa9628fa116ea30e0c0bc32f08c961859dd9
CJ Virtucio <cvirtucio@mitre.org>
2018-05-18 16:03:27 -0400

    added govendor

--- cygwin/.shell_init/go_config/aliases.sh
+++ cygwin/.shell_init/go_config/aliases.sh
@@ -7,4 +7,14 @@ function mygo {
     printf "Resetting \$PATH.";
 }
 
+function mygovendor {
+    MINGW_GIT='/cygdrive/c/Tools/Git/mingw64/bin';
+    PATH=$MINGW_GIT:$PATH;
+    printf "Running govendor with the Windows installation of Git.\n";
+    $GOBIN/govendor $@;
+    PATH=`echo $PATH | sed "s,$MINGW_GIT\:,,g"`;
+    printf "Resetting \$PATH.";
+}
+
 alias go=mygo
+alias govendor=mygovendor
