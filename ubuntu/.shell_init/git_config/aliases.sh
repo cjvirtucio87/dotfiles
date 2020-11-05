20dfb6f4cd78cdf72f94f07bec27745bdc8f9dd6
CJ Virtucio <cjv287@gmail.com>
2020-11-05 09:34:44 -0500

    replaced aliases with functions; added worktree functions

--- ubuntu/.shell_init/git_config/aliases.sh
+++ ubuntu/.shell_init/git_config/aliases.sh
@@ -1,7 +1,39 @@
-alias gpom="git push origin master"
-alias gpoc="git push origin"
-alias gpocs="git push origin --set-upstream"
-alias gcm="git commit -m"
-alias gaa="git add -A"
-alias gspushm="git stash push -m"
-alias gspop="git stash pop"
+function gaa {
+  # stage all files
+  git add -A "$@"
+}
+
+function gcm {
+  # commit all files with message as first argument
+  git commit -m "$1"
+}
+
+function gpoc {
+  # push commits to origin
+  git push origin "$@"
+}
+
+function gpocs {
+  # push commits to origin and set upstream branch to first argument
+  git push origin --set-upstream "$1"
+}
+
+function gpom {
+  # push commits to master
+  git push origin master "$@"
+}
+
+function gw {
+  # shortcut to worktree operations
+  git worktree "$@"
+}
+
+function gwac {
+  # add worktree at path ${HOME}/git/<current-directory-name>-<first-argument>,
+  git worktree add "${HOME}/git/${PWD##*/}-$1" --checkout "$1"
+}
+
+function gwrc {
+  # remove worktree at path ${HOME}/git/<current-directory-name>-<first-argument>
+  git worktree remove "${HOME}/git/${PWD##*/}-$1"
+}
