a52c649e9fd4eb567c8b24cf101e4af3fa9a0df9
CJ Virtucio <cjv287@gmail.com>
2021-02-22 17:27:00 -0500

    added shell_init/scripts; added convo_format.sh

---
+++ ubuntu/.shell_init/scripts/bin/convo_format.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+### A simple script to format conversations for JIRA.
+###
+### Arguments:
+###   filepath: the path to the file containing the conversation to format
+###   convo_medium: the conversation's medium (slack|email)
+###
+### Usage:
+###   convo_format.sh '/tmp/convo.txt' 'email'
+
+function main {
+  local filepath="$1"; shift
+  local convo_medium="$1"; shift
+
+  if [[ -z "${filepath}" ]] || [[ -z "${convo_medium}" ]]; then
+    >&2 echo "Invalid arguments (filepath: ${filepath}, convo_medium: ${convo_medium})"
+    exit 1
+  fi
+
+  local regex_pattern
+  case "${convo_medium}" in
+    'email')
+      regex_pattern='^(From|Sent|To|Subject):'
+      ;;
+    'slack')
+      regex_pattern='(^[a-zA-Z :_]+([0-9][0-9]:[0-9][0-9] [AP]M$|[><]{0,1} [0-9] (year|month|day|hour|minute|second)s{0,1} ago)$)'
+      ;;
+    *)
+      ;;
+  esac
+
+  sed --regexp-extended "s/${regex_pattern}/*\1*/g" "${filepath}"
+}
+
+main "$@"
