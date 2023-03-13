602a791c0d26cbcaf79402a5ea18982f9339197f
CJ Virtucio <cjv287@gmail.com>
2023-03-12 20:29:48 -0400

    autocomplete shortcut for omnisharp; checksum validation in install_omnisharp.sh

--- ubuntu/.cjvirtucio87nvim/lua/lspconfig-cfg.lua
+++ ubuntu/.cjvirtucio87nvim/lua/lspconfig-cfg.lua
@@ -1,3 +1,10 @@
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  -- see: https://github.com/neovim/nvim-lspconfig/tree/4bb0f1845c5cc6465aecedc773fc2d619fcd8faf#suggested-configuration
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
 require'lspconfig'.omnisharp.setup {
-  cmd = { os.getenv("HOME") .. "/.omnisharp/current/OmniSharp" }
+  cmd = { os.getenv("HOME") .. "/.omnisharp/current/OmniSharp" },
+  on_attach = on_attach
 }
--- ubuntu/.cjvirtucio87sh/scripts/bin/install_omnisharp.sh
+++ ubuntu/.cjvirtucio87sh/scripts/bin/install_omnisharp.sh
@@ -10,15 +10,24 @@ set -eo pipefail
 ### Usage:
 ###   <Options> install_omnisharp.sh
 
-OMNISHARP_VERSION="${OMNISHARP_VERSION:-1.39.4}"
-readonly OMNISHARP_VERSION
+readonly OMNISHARP_VERSION="1.39.4"
+readonly OMNISHARP_CHECKSUM=84299e917cdfdbd9540da9506bee6e7b9a35ca3958e12ec12c19a3ec36a51ff5
 readonly VERSIONED_DIR="${HOME}/.omnisharp/${OMNISHARP_VERSION}"
 readonly CURRENT_DIR="${HOME}/.omnisharp/current"
+
+function cleanup {
+  log "cleaning up"
+  if [[ -n "${TMP_FILE}" ]]; then
+    rm -f "${TMP_FILE}"
+  fi
+  log "done cleaning up"
+}
 
 function help {
   local line
   while read -r line; do
     if [[ "${line}" =~ ^### ]]; then
+      # shellcheck disable=SC2001
       sed 's,^###,,g' <<<"${line}"
     fi
   done < "$0"
@@ -37,14 +46,39 @@ function main {
   mkdir -p "${VERSIONED_DIR}"
 
   log "installing ${VERSIONED_DIR}"
+  TMP_FILE="$(mktemp --suffix '.tar.gz')"
+  trap cleanup EXIT
+
   curl \
     --location \
+    --output "${TMP_FILE}" \
     "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v${OMNISHARP_VERSION}/omnisharp-linux-x64-net6.0.tar.gz" \
-    | tar \
-        --extract \
-        --gunzip \
-        --directory "${VERSIONED_DIR}" \
-        .
+
+  local tmp_file_checksum
+  tmp_file_checksum="$(sha256sum < "${TMP_FILE}" | awk '{print $1}' | tr -d '\n')"
+
+  if [[ "${OMNISHARP_CHECKSUM}" != "${tmp_file_checksum}" ]]; then
+    log "OmniSharp checksum mismatch; expected ${OMNISHARP_CHECKSUM}, got ${tmp_file_checksum}"
+    return 1
+  fi
+
+  local versioned_checksum_file="${VERSIONED_DIR}/.checksum"
+  if [[ -f "${versioned_checksum_file}" ]]; then
+    local versioned_checksum
+    versioned_checksum="$(<"${versioned_checksum_file}")"
+
+    if [[ "${OMNISHARP_CHECKSUM}" == "${versioned_checksum}" ]]; then
+      log "checksum matches; already installed"
+      return
+    fi
+  fi
+
+  tar \
+    --extract \
+    --gunzip \
+    --file "${TMP_FILE}" \
+    --directory "${VERSIONED_DIR}" \
+    .
 
   if [[ -L "${CURRENT_DIR}" ]]; then
     log "unlinking ${CURRENT_DIR}"
@@ -52,6 +86,7 @@ function main {
   fi
 
   log "linking ${CURRENT_DIR} to ${VERSIONED_DIR}"
+  echo -n "${OMNISHARP_CHECKSUM}" > "${versioned_checksum_file}"
   ln -s "${VERSIONED_DIR}" "${CURRENT_DIR}"
 }
 
