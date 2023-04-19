484ba23df67f674966e82ef39fb40bd2aaa0446d
Christopher Virtucio <cjv287@gmail.com>
2023-04-19 06:53:29 -0400

    anon functions

--- osx/.cjvirtucio87nvim/lua/appearance.lua
+++ osx/.cjvirtucio87nvim/lua/appearance.lua
@@ -1,25 +1,20 @@
 vim.opt.termguicolors = true
 
--- cursor style
 vim.opt.guicursor = 'n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor,a:blinkon100'
-
-function hiCursor() 
-  vim.api.nvim_set_hl(0, "Cursor", {fg='#cc9900', bg='#339966'})
-  vim.api.nvim_set_hl(0, "CursorReset", {fg='white', bg='white'})
-end
 
 vim.api.nvim_create_autocmd("ColorScheme", {
     pattern = "*",
-    callback = hiCursor,
+    callback = function()
+      vim.api.nvim_set_hl(0, "Cursor", {fg='#cc9900', bg='#ff3300'})
+      vim.api.nvim_set_hl(0, "CursorReset", {fg='white', bg='white'})
+    end
 })
-
-function resetHi()
-  vim.opt.guicursor = 'a:ver25-CursorReset,a:blinkon100'
-end
 
 vim.api.nvim_create_autocmd("VimLeave", {
     pattern = "*",
-    callback = resetHi, 
+    callback = function()
+      vim.opt.guicursor = 'a:ver25-CursorReset,a:blinkon100'
+    end
 })
 
 vim.cmd[[colorscheme nord]]
