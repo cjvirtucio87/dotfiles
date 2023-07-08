97f0fa184f007bf285cc9151cfb7aa09efb0c265
CJ Virtucio <cjv287@gmail.com>
2023-07-08 18:14:16 -0400

    indentation

--- ubuntu/.cjvirtucio87nvim/lua/misc.lua
+++ ubuntu/.cjvirtucio87nvim/lua/misc.lua
@@ -15,23 +15,15 @@ if vim.fn.has("wsl") == 1 then
       name = "wl-clipboard (wsl)",
       copy = {
         ["+"] = "wl-copy --foreground --type text/plain",
-	["*"] = "wl-copy --foreground --primary --type text/plain",
+        ["*"] = "wl-copy --foreground --primary --type text/plain",
       },
       paste = {
-	["+"] = (function()
-	  return vim.fn.systemlist(
-	    'wl-paste --no-newline | sed -e "s/\r$//"',
-	    {''},
-	    1
-	  )
-	end),
-	["*"] = (function()
-	  return vim.fn.systemlist(
-	    'wl-paste --primary --no-newline | sed -e "s/\r$//"',
-	    {''},
-	    1
-	  )
-	end)
+        ["+"] = (function()
+          return vim.fn.systemlist('wl-paste --no-newline | sed -e "s/\r$//"', {''}, 1)
+        end),
+        ["*"] = (function()
+          return vim.fn.systemlist('wl-paste --primary --no-newline | sed -e "s/\r$//"', {''}, 1)
+        end)
       },
       cache_enabled = true
     }
