ed8f2eb0f075a51d4dbbc880d4536d132e43165c
Christopher Virtucio <cjvirtucio@cjvautomation.home.cjvirtuc.io>
2023-12-10 06:56:39 -0500

    added rule for hcl

--- ubuntu/.cjvirtucio87nvim/lua/format.lua
+++ ubuntu/.cjvirtucio87nvim/lua/format.lua
@@ -115,3 +115,16 @@ vim.api.nvim_create_autocmd(
     end
   }
 )
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    pattern = "hcl",
+    callback = function()
+      vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
+      vim.opt.shiftwidth = 2
+      vim.opt.expandtab = true
+    end
+  }
+)
