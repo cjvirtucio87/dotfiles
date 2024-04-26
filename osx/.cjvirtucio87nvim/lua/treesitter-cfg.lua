e4ebbdab73c113b100effd54a8855caae7ea004f
Christopher Virtucio <cjv287@gmail.com>
2024-04-26 08:21:50 -0400

    added tressitter requirements

--- osx/.cjvirtucio87nvim/lua/treesitter-cfg.lua
+++ osx/.cjvirtucio87nvim/lua/treesitter-cfg.lua
@@ -2,4 +2,8 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true
   }
+
+  -- treesitter requirements
+  -- see: https://stackoverflow.com/a/75092074
+  ensure_installed = { 'c', 'help', 'vim' }
 }
