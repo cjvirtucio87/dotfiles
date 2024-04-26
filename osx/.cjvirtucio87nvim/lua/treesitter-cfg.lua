795752a18e7f8c40e04ca8768ec649410f1b61a5
Christopher Virtucio <cjv287@gmail.com>
2024-04-26 08:23:47 -0400

    comma on setup call

--- osx/.cjvirtucio87nvim/lua/treesitter-cfg.lua
+++ osx/.cjvirtucio87nvim/lua/treesitter-cfg.lua
@@ -1,9 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true
-  }
+  },
 
   -- treesitter requirements
   -- see: https://stackoverflow.com/a/75092074
-  ensure_installed = { 'c', 'help', 'vim' }
+  ensure_installed = { 'c', 'help', 'vim' },
 }
